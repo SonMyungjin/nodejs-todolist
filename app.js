fef19f0aaa8f6cbcd26b1cb9f29f7d03a273b295
Son Myungjin <shonmyungjin@naver.com>
2021-12-14 18:57:46 +0900

    Refactor: MongoDB / controller / routes / pug->ejs

--- app.js
+++ app.js
@@ -1,48 +1,34 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require('path');
 
 const app = express();
 
-let todoLists = [];
 
 app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set("views", "./views");
 
-app.use(express.static('public'));
+app.use("/public", express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.render('index', {todoListTitle: '오늘의 할 일 : ' + todoLists.length, todoLists})
-});
 
-app.post('/add_list', (req, res) => {
-    const newContent = req.body.content;
-    console.log(newContent + '추가');
-    todoLists.push(newContent);
-    res.redirect('/');
-});
+const router = require("./routes/index");
+app.use(router);
 
-app.get('/delete_list/:id', (req,res) => {
-    const deleteContent = req.params.id;
-    console.log(deleteContent + '삭제');
-    todoLists = todoLists.filter((value) => value != deleteContent);
-    res.redirect('/');
-});
+  
+mongoose.connect("mongodb://root:1234@localhost:27017/nodejs?authSource=admin", {
+    useNewUrlParser: true, useUnifiedTopology: true}, function(err){
+    if(err){
+        console.error("mongoDB Connection Error!", err);
+    }
+    console.log("mongoDB Connected!");
+    
+    // Server Open
+    app.listen(app.get('port'), () => {
+        console.log(app.get('port'), '번 포트 연결');
+    });
+});  
 
-app.get('/open_update/:id', (req, res) => {
-    res.render('update', {prevContent: req.params.id});
-});
-
-app.post('/update_list', (req, res) => {
-    let prevContent = req.body.prevContent;
-    let newContent = req.body.newContent;
-    let index = todoLists.indexOf(prevContent);
-    todoLists.splice(index, 1, newContent);
-    console.log(prevContent + '을(를)' + newContent + '(으)로 수정');
-    res.redirect('/');
-});
-
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트 연결');
-});
